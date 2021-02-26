a163a9525894e6e29501cae479628e3230c8a3e6
madeleine <yuema0415@gmail.com>
2021-02-26 17:06:49 -0500

    created charts version1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './App.css';
 import React, {Component} from 'react';
-import Addresses from "./components/Addresses";
 import AddAddress from "./components/AddAddress";
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 import './main.css';
 import {
-    VictoryChart,
-    VictoryZoomContainer,
-    VictoryLine,
-    VictoryBrushContainer,
+    createContainer,
+    VictoryArea,
     VictoryAxis,
-    VictoryTheme,
-    VictoryVoronoiContainer,
-    VictoryTooltip,
-    createContainer
+    VictoryBrushContainer,
+    VictoryChart,
+    VictoryLine,
+    VictoryTooltip
 } from 'victory';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from './index';
-
-import {ResponsiveLine} from '@nivo/line'
-
-
-const data = [{'x': new Date(2021, 1, 4, 14, 0, 0), 'y': 2695.1389354868006},
-    {'x': new Date(2021, 1, 5, 14, 0, 0), 'y': 2395.1389354868006}]
 
 
 class App extends Component {
@@ -32,6 +22,7 @@ class App extends Component {
             addresses: [],
             error: null,
             datesWithBalances: [],
+            currentBalance: null
         }
 
         this.btcUsdApiBase = 'http://127.0.0.1:5000/api/btc?address='
@@ -44,7 +35,9 @@ class App extends Component {
                 (result) => {
                     this.setState({
                         isLoaded: true,
-                        datesWithBalance: this.formatData(result)
+                        datesWithBalance: this.formatData(result),
+                        address: address,
+                        currentBalance: this.getCurrentBalance(result)
                     });
                     console.log(this.state.datesWithBalance)
                 },
@@ -61,6 +54,22 @@ class App extends Component {
 
     }
 
+    formatData(inputData) {
+        for (let i = 0; i < inputData.length; i++) {
+            inputData[i]['x'] = new Date(inputData[i]['x'])
+            inputData[i]['y'] = Math.round(inputData[i]['y'])
+        }
+        return inputData
+    }
+
+    getCurrentBalance(inputData) {
+        return inputData[inputData.length - 1]['y']
+    }
+
+    addAddress = (address) => {
+        this.fetchBitcoinTranscationDataWithAPI(address)
+    }
+
     handleZoom(domain) {
         this.setState({selectedDomain: domain})
     }
@@ -69,22 +78,8 @@ class App extends Component {
         this.setState({zoomDomain: domain});
     }
 
-
-    formatData(inputData) {
-        for (let i = 0; i < inputData.length; i++) {
-            inputData[i]['x'] = new Date(inputData[i]['x'])
-        }
-
-        return inputData
-    }
-
-
-    addAddress = (address) => {
-        this.fetchBitcoinTranscationDataWithAPI(address)
-    }
-
     render() {
-        const {isLoaded, address, datesWithBalance} = this.state;
+        const {isLoaded, address, datesWithBalance, currentBalance} = this.state;
         const VictoryZoomVoronoiContainer = createContainer('zoom', 'voronoi')
 
         if (!isLoaded) {
@@ -98,43 +93,117 @@ class App extends Component {
         return (
             <React.Fragment>
                 <div className='height'>
-                    <VictoryChart width={1000} height={500} scale={{x: 'time'}} containerComponent={
-                        <VictoryZoomVoronoiContainer responsive={false}
-                                                     zoomDimension='x'
-                                                     zoomDomain={this.state.zoomDomain}
-                                                     onZoomDomainChange={this.handleZoom.bind(this)}
-                                                     labels={({datum}) => `y: ${datum.y}, x:${datum.x}`}
-                                                     labelComponent={<VictoryTooltip/>}
-                        />
-                    }
-                    >
-                        <VictoryLine style={{data: {stroke: 'tomato'}}}
-                                     data={this.state.datesWithBalance}
-                        />
-                    </VictoryChart>
+                    <Row>
+                        <Col lg={9}>
+                            <div className='main-chart'>
+                                <div className='chart-title'>Bitcoin Wallet Balance (USD)</div>
+                                <div className='small-title'>Address: {this.state.address}</div>
+                                <VictoryChart width={1000} height={500} scale={{x: 'time'}} containerComponent={
+                                    <VictoryZoomVoronoiContainer zoomDimension='x'
+                                                                 zoomDomain={this.state.zoomDomain}
+                                                                 onZoomDomainChange={this.handleZoom.bind(this)}
+                                                                 labels={({datum}) => `$${datum.y}
+${(datum.x).toLocaleString()}`}
+                                                                 labelComponent={<VictoryTooltip
+                                                                     flyoutStyle={{stroke: '#41b6c4', fill: '#41b6c4'}}
+                                                                     flyoutWidth={200}
 
-                    <VictoryChart width={1000} height={300} scale={{x: 'time'}} containerComponent={
-                        <VictoryBrushContainer responsive={false}
-                                               brushDimension='x'
-                                               brushDomain={this.state.selectedDomain}
-                                               onBrushDomainChange={this.handleBrush.bind(this)}
-                        />
-                    }
-                    >
-                        <VictoryAxis tickValues={[new Date('2020-12-12 23:00:00'), new Date('2021-02-25 16:00:00')]}
-                                     tickFormat={(x) => new Date(x).getMonth()}
+                                                                 />}
+                                    />
+                                }
+                                >
+                                    <VictoryAxis crossAxis
+                                                 style={{
+                                                     axis: {
+                                                         stroke: '#d1d9e0'
+                                                     },
+                                                     tickLabels: {
+                                                         fill: '#d1d9e0',
+                                                     },
+                                                     grid: {
+                                                         stroke: ({tick}) => tick > 1 ? "#adb5bd" : "#adb5bd",
+                                                         opacity: 0.1
+                                                     }
 
-                        />
+                                                 }}
+                                                 padding={{bottom: 20}}
+                                    />
+                                    <VictoryAxis dependentAxis crossAxis
+                                                 label='USD'
+                                                 style={{
+                                                     axis: {
+                                                         stroke: '#d1d9e0'
+                                                     },
+                                                     tickLabels: {
+                                                         fill: '#d1d9e0'
+                                                     },
+                                                     grid: {
+                                                         stroke: ({tick}) => tick > 5000 ? "#adb5bd" : "#adb5bd",
+                                                         opacity: 0.1
+                                                     }
 
-                        <VictoryLine
-                            style={{
-                                data: {stroke: "#c43a31"},
-                                parent: {border: "1px solid #ccc"}
-                            }}
-                            data={this.state.datesWithBalance}
+                                                 }}/>
+                                    <VictoryArea style={{data: {fill: '#7fcdbb', fillOpacity: 0.1, stroke: '#41b6c4'}}}
+                                                 data={this.state.datesWithBalance}
+                                    />
+                                </VictoryChart>
 
-                        />
-                    </VictoryChart>
+
+                                <VictoryChart width={1000} height={200} scale={{x: 'time'}} containerComponent={
+                                    <VictoryBrushContainer brushDimension='x'
+                                                           brushDomain={this.state.selectedDomain}
+                                                           onBrushDomainChange={this.handleBrush.bind(this)}
+                                                           brushStyle={{fill: '#7fcdbb', opacity: 0.4}}
+                                    />
+                                }
+                                >
+                                    <VictoryAxis
+                                        tickValues={[new Date('2020-12-12 23:00:00'), new Date('2021-02-25 16:00:00')]}
+                                        tickFormat={(x) => new Date(x).getMonth()} style={{
+                                        axis: {
+                                            stroke: '#d1d9e0'
+                                        },
+                                        tickLabels: {
+                                            fill: '#d1d9e0'
+                                        }
+
+                                    }}
+
+                                    />
+
+                                    <VictoryAxis dependentAxis
+                                                 style={{
+                                                     axis: {
+                                                         stroke: '#d1d9e0'
+                                                     },
+                                                     tickLabels: {
+                                                         fill: '#d1d9e0'
+                                                     }
+
+                                                 }}/>
+
+                                    <VictoryLine
+                                        style={{
+                                            data: {stroke: "#41b6c4"},
+                                            parent: {border: "1px solid #41b6c4"}
+                                        }}
+                                        data={this.state.datesWithBalance}
+
+                                    />
+                                </VictoryChart>
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+                            <div className='main-chart'>
+                                <div className='balance'>Current Balance: {this.state.currentBalance} usd</div>
+                                <div className='balance'>Current Exchange Rate:</div>
+                                <div className='balance'>Hourly:</div>
+                                <div className='balance'>Weekly:</div>
+                                <div className='balance'>Yearly:</div>
+                                <div className='balance'>Transaction History:</div>
+                            </div>
+                        </Col>
+                    </Row>
                 </div>
             </React.Fragment>
 
